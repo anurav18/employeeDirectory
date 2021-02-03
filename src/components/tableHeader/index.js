06c49c800914705556cf22f5ea861745d67e5eff
Anu Rav <anusharavva@gmail.com>
2021-02-02 22:06:32 -0800

    re-added files

---
+++ src/components/tableHeader/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function tableHeader(){
+return(
+
+    <tr>
+    <th scope="col">Image</th>
+    <th scope="col">Name</th>
+    <th scope="col">Phone</th>
+    <th scope="col">Email</th>
+    <th scope="col">DOB</th>
+  </tr>
+
+)
+}
+
+export default tableHeader;
